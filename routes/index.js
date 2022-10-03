077b140d8c57134e4500b57fa878658238e5c7ef
Yin-Siangl <ymao6@my.centennialcollege.ca>
2022-10-03 19:41:02 -0400

    try

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.get('/services', function(req, res, next) {
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+  res.render('contactme', { title: 'Contact'});
 });
 
 module.exports = router;
