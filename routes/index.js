a39bd8941cdcb82deb98529e5cfd18f8f2aaceab
Yin-Siangl <ymao6@my.centennialcollege.ca>
2022-10-03 18:56:14 -0400

    try

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,16 @@ router.get('/', function(req, res, next) {
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home'});
+  
 });
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About'});
+  <body>
+  Hi My name is Yin-Siang Mao
+  Currently a student of Software Engineering Technology - AI
+  </body>
 });
 
 /* GET Products page. */
